de16cc083bfca4fd240b543f577c7664333a9d3e
Alexey Rogachevskiy <sfalexrog@gmail.com>
2019-11-14 22:37:15 +0300

    docker: Add kinetic builder

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,10 @@
 echo "--- Registering binfmt_misc support for arm"
 docker run --rm --privileged sfalexrog/qemu-register:v4.1.0
 
-#echo "--- Building for Kinetic"
-#pushd kinetic
-#docker build --rm --tag=sfalexrog/multipack:kinetic .
-#popd
+echo "--- Building for Kinetic"
+pushd kinetic-builder
+docker build --rm --tag=sfalexrog/multipack:kinetic .
+popd
 
 echo "--- Building for Melodic"
 pushd melodic-builder
